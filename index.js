6309fac6acee584c8ef1857189b1048029e33420
Justin Morrow <morrowjustin19@gmail.com>
2022-06-19 22:36:58 -0700

    updating

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require ('inquirer');
+const fs = require('fs');
+const util = require ('util');
+const Employee = require("./lib/Engineer");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Engineer");
+const Manager = require("./lib/Engineer");
