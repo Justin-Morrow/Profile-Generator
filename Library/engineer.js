36d166c88cd15dbbca3d3f039a7079368c370a0c
Justin Morrow <morrowjustin19@gmail.com>
2022-06-22 23:23:13 -0700

    updating

--- Library/engineer.js
+++ Library/engineer.js
@@ -5,12 +5,12 @@ const employee = require("./employee");
 
 class Engineer extends employee {
     constructor(name, id, email, gitHub) {
-        super(name, id, email);
-        this.gitHub = gitHubUserName;
+        super(name, id, email)
+        this.gitHub = gitHub
     }
 
     getGitHub() {
-        return this.getGitHub;
+        return this.gitHub;
     };
 
     getRole() {
